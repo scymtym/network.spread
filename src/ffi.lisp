5db919769411575cd10264aa859cd4f140617e90
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-05-17 23:19:28 +0200

    Added additional libspread search location in src/ffi.lisp * src/ffi.lisp (libspread): also search in   /vol/default/lib/libspread.so.2.0

--- src/ffi.lisp
+++ src/ffi.lisp
@@ -26,6 +26,7 @@
 (cffi:define-foreign-library libspread
   (:unix (:or "/vol/spread/lib/libspread.so.2.0"
 	      "/vol/xcf/lib/libspread.so.2.0"
+	      "/vol/default/lib/libspread.so.2.0"
 	      "libspread.so"
 	      "libspread.so.1"))
   (t     (:default "libspread")))
