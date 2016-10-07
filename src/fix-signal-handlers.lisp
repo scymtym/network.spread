f81750178b8eda550b353779f5abff8721be211b
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2016-10-07 23:39:53 +0200

    Improved signal handler restoration in src/fix-signal-handlers.lisp
    Previously, signal handlers were only restored after connecting
    successfully. However, restoring handlers in case of a non-local control
    transfer is just as important.
    
    * src/fix-signal-handlers.lisp (header): updated copyright
      (fix-signal-handlers): turned documentation string into comment
      (connect :around t): changed qualifier :after -> :around; restore
      signal handlers in an `unwind-protect' cleanup; turned documentation
      string into comment

--- src/fix-signal-handlers.lisp
+++ src/fix-signal-handlers.lisp
@@ -1,18 +1,21 @@
 ;;;; fix-signal-handlers.lisp --- Undo Spread's changes to SBCL's signal handlers.
 ;;;;
-;;;; Copyright (C) 2011, 2012 Jan Moringen
+;;;; Copyright (C) 2011-2016 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
 (cl:in-package #:network.spread)
 
 (defun fix-signal-handlers ()
-  "Restore signal handlers for SIGINT and SIGPIPE to their original
-states."
+  ;; Restore signal handlers for SIGINT and SIGPIPE to their original
+  ;; states.
   (sb-unix::enable-interrupt sb-unix::sigint  #'sb-unix::sigint-handler)
   (sb-unix::enable-interrupt sb-unix::sigpipe #'sb-unix::sigpipe-handler))
 
-(defmethod connect :after ((daemon t))
-  "After the Spread connection to DAEMON has been created, restore the
-signal handlers of the current thread to their original state."
-  (fix-signal-handlers))
+(defmethod connect :around ((daemon t))
+  ;; After the Spread connection to DAEMON has been created, restore
+  ;; the signal handlers of the current thread to their original
+  ;; state.
+  (unwind-protect
+       (call-next-method)
+    (fix-signal-handlers)))
