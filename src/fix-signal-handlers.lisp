842993ab65decee371bf548b90e0795fe254fcb1
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2020-09-18 08:19:33 +0200

    Only touch SIGPIPE handler if it exists in src/fix-signal-handlers.lisp
    * src/fix-signal-handlers.lisp (header): updated copyright
      (toplevel): determine whether `sb-unix::sigpipe-handler' exists
      (fix-signal-handlers): omit code for `sb-unix::sigpipe-handler' if
      it does not exists

--- src/fix-signal-handlers.lisp
+++ src/fix-signal-handlers.lisp
@@ -1,15 +1,22 @@
 ;;;; fix-signal-handlers.lisp --- Undo Spread's changes to SBCL's signal handlers.
 ;;;;
-;;;; Copyright (C) 2011-2016 Jan Moringen
+;;;; Copyright (C) 2011-2016, 2020 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
 (cl:in-package #:network.spread)
 
+;;; http://jcsu.jesus.cam.ac.uk/~csr21/papers/features.pdf
+(eval-when (:compile-toplevel :execute)
+  (when (and (find-package '#:sb-unix)
+             (find-symbol (string '#:sigpipe-handler) '#:sb-unix))
+    (pushnew 'sb-unix-sigpipe-handler *features*)))
+
 (defun fix-signal-handlers ()
   ;; Restore signal handlers for SIGINT and SIGPIPE to their original
   ;; states.
   (sb-unix::enable-interrupt sb-unix::sigint  #'sb-unix::sigint-handler)
+  #+network.spread::sb-unix-sigpipe-handler
   (sb-unix::enable-interrupt sb-unix::sigpipe #'sb-unix::sigpipe-handler))
 
 (defmethod connect :around ((daemon t))
